ec1476bdfb31fac3fd1c58deb5b78adbaf4ccbef
ajreid <axr490@miami.edu>
2020-03-04 09:56:42 -0500

    added more fields to modal and created table that updates on each category

--- client/src/components/NewBudgetModal/index.js
+++ client/src/components/NewBudgetModal/index.js
@@ -1,64 +1,152 @@
-import React from "react";
+import React, { Component } from "react";
 import { Input } from "../Form";
 import "./style.css";
 import { Container, Row } from "../Grid";
 
-export default function NewBudgetModal() {
-    return (
-        <div>
-            <div id="new-budget-modal" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-lg" role="document">
-                    <div className="modal-content">
+class NewBudgetModal extends Component {
 
-                        <div className="modal-header">
-                            <h5 className="modal-title">Create New Budget</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+    state = {
+        newBudgetName: "",
+        newBudgetTotal: 0,
+        newCategoryName: "",
+        newCategoryAmount: 0,
+        categoryData: [],
+        testContent: []
+    }
+
+    addCategoryData = () => {
+
+        let newCategoryData = this.state.categoryData;
+        let content = [];
+
+        newCategoryData.push(
+            {
+                name: this.state.newCategoryName,
+                amount: this.state.newCategoryAmount
+            });
+
+        newCategoryData.forEach(category => {
+            content.push(
+                (
+                    <tr>
+                        <td>
+                            <button type="button" className="category-delete">
                                 <span aria-hidden="true">&times;</span>
                             </button>
+                            {category.name}
+                        </td>
+                        <td>{category.amount}</td>
+                    </tr>
+                )
+            )
+        });
+
+        this.setState({ categoryData: newCategoryData, testContent: content, newCategoryName: "", newCategoryAmount: "" });
+
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="new-budget-modal" className="modal fade" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg" role="document">
+                        <div className="modal-content">
+
+                            <div className="modal-header">
+                                <h5 className="modal-title">Create New Budget</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <div className="modal-body">
+                                <form>
+                                    <Container>
+                                        <div className="row modal-content-group">
+                                            <div className="form-group col-md-8">
+                                                <label htmlFor="budget-name" className="col-form-label">Budget Name</label>
+                                                <input
+                                                    id="budget-name"
+                                                    name="newBudgetName"
+                                                    onChange={this.handleInputChange}
+                                                    value={this.state.newBudgetName}
+                                                    type="text"
+                                                    placeholder="Ex: Lifestyle"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="form-group col-md-4">
+                                                <label htmlFor="budget-total" className="col-form-label">Total ($)</label>
+                                                <input
+                                                    id="budget-total"
+                                                    name="newBudgetTotal"
+                                                    onChange={this.handleInputChange}
+                                                    value={this.state.newBudgetTotal}
+                                                    type="text"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="row modal-content-group">
+                                            <div className="form-group col-md-6">
+                                                <label htmlFor="category-name" className="col-form-label">Category Name</label>
+                                                <input
+                                                    id="category-name"
+                                                    name="newCategoryName"
+                                                    onChange={this.handleInputChange}
+                                                    value={this.state.newCategoryName}
+                                                    type="text"
+                                                    placeholder="Ex: Food"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <div className="form-group col-md-3">
+                                                <label htmlFor="category-amount" className="col-form-label">Amount ($)</label>
+                                                <input
+                                                    id="category-amount"
+                                                    name="newCategoryAmount"
+                                                    onChange={this.handleInputChange}
+                                                    value={this.state.newCategoryAmount}
+                                                    type="text"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                            <button
+                                                id="btn-add-category"
+                                                type="button"
+                                                onClick={this.addCategoryData}
+                                                class="btn btn-primary">Add</button>
+                                        </div>
+                                        <Row>
+                                            <table id="category-table" width="100%">
+                                                <tbody>
+                                                    <tr>
+                                                        <th>Name</th>
+                                                        <th>Amount</th>
+                                                    </tr>
+                                                    {this.state.testContent}
+                                                </tbody>
+                                            </table>
+                                        </Row>
+
+                                    </Container>
+                                </form>
+                            </div>
+
                         </div>
-
-                        <div className="modal-body">
-                            <form>
-                                <Container>
-                                    <div className="row modal-content-group">
-                                        <div className="form-group col-md-8">
-                                            <label htmlFor="budget-name" className="col-form-label">Budget Name</label>
-                                            <input type="text" className="form-control" id="budget-name"></input>
-                                        </div>
-                                        <div className="form-group col-md-4">
-                                            <label htmlFor="budget-total" className="col-form-label">Total ($)</label>
-                                            <input type="text" className="form-control" id="budget-total"></input>
-                                        </div>
-                                    </div>
-                                    <div className="row modal-content-group">
-                                        <div className="form-group col-md-6">
-                                            <label htmlFor="category-name" className="col-form-label">Category Name</label>
-                                            <input type="text" className="form-control" id="category-name"></input>
-                                        </div>
-                                        <div className="form-group col-md-3">
-                                            <label htmlFor="category-amount" className="col-form-label">Amount ($)</label>
-                                            <input type="text" className="form-control" id="budget-total"></input>
-                                        </div>
-                                        <button id="btn-add-category" type="button" class="btn btn-primary">Add</button>
-                                    </div>
-                                    <Row>
-                                        <table width="100%">
-                                            <tbody>
-                                                <tr>
-                                                    <th>Name</th>
-                                                    <th>Amount</th>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </Row>
-
-                                </Container>
-                            </form>
-                        </div>
-
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default NewBudgetModal;
