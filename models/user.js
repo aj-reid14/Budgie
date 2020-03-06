9cdb348d25dc1edf0e9b47b34fd337cf003291d9
ajreid <axr490@miami.edu>
2020-03-06 00:25:48 -0500

    created 'user' schema that handles user login info and budget info

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    budgets: [{
+        budgetName: {type: String, required: true},
+        budgetTotal: {type: Number, min: 1, required: true},
+        categories: [{
+            categoryName: {type: String, required: true},
+            categoryAmount: {type: Number, min: 1, required: true}
+        }],
+        createdAt: {type: Date, default: Date.now}
+    }],
+    joined: { type: Date, default: Date.now }
+  });
+  
+  const User = mongoose.model("User", userSchema);
+  
+  module.exports = User;
