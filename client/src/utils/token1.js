7c704898c07014be60e33172a81f6f1f2b301a7d
Patrick Heron <hpa434@gmail.com>
2020-03-06 23:20:02 -0500

    added route file to have a base to work off of for authentication, need to find a way to connect modal to user schema

--- client/src/utils/token1.js
+++ client/src/utils/token1.js
@@ -1,7 +1,25 @@
+const config = require('config');
 const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 const fs = require('fs');
 let payloadObj = "../../../models/user.js"
 let PRIV_KEY = "./default.json"
 
 let  signedjwt = jwt.sign(payloadObj, PRIV_KEY, { algorithm: 'RS256'});
   
+const User = mongoose.model('User', UserSchema);
+
+//function to validate user 
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User; 
+exports.validate = validateUser;
+module.exports = signedjwt
---
+++ client/src/utils/verify.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const signedjwt = require("./token.js")
+let PRIV_KEY = "./default.json"
+
+
+
+// console.log(signedjwt)
+
+jwt.verify(signedjwt, PUB_KEY, { algorithms: ['RS256'] }, (err, payload) => {
+
+  if (err) {
+    switch (err.name) {
+      case 'TokenExpiredError':
+        console.log('Whoops, your token has expired!');
+        break;
+      case 'JsonWebTokenError':
+        console.log('That JWT is malformed!');
+        break;    
+      default:
+        console.log(err.name);
+        break;
+    }
+  } else {
+    console.log('Your JWT was successfully validated!');
+  }
+
+  // if (err == 'TokenExpiredError') {
+  //   console.log('Whoops, your token has expired!');
+  // }
+  
+  // if (err == 'JsonWebTokenError') {
+  //   console.log('That JWT is malformed!');
+  // }
+  
+  // if (err === null) {
+  // console.log('Your JWT was successfully validated!');
+  // }
+
+  // Both should be the same
+  // console.log(signedjwt);
+  // console.log(payload);
+});
