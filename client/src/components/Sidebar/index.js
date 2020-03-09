d32a9affe911dd89ff5aff5c7a5c2dd6267280d1
ajreid <axr490@miami.edu>
2020-03-09 19:13:10 -0400

    added template class that will display created budgets on the sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
-export default function Sidebar() {
-    return (
-        <div id="sidebar">
-            <div id="create-budget" data-toggle="modal" data-target="#new-budget-modal"></div>
-        </div>
-    )
+class Sidebar extends Component {
+
+    render() {
+
+        return (
+            <div id="sidebar">
+                {this.props.children}
+                <div className="user-bdgt"></div>
+                <div id="create-budget" data-toggle="modal" data-target="#new-budget-modal"></div>
+            </div>
+        )
+    }
 }
+
+export default Sidebar;
