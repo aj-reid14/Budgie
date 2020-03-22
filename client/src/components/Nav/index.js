cd2e1be78620d6e76bda2bbe0cb6e43147c1ec80
ruok27 <jakxii27@gmail.com>
2020-03-21 22:27:47 -0400

    Navbar greets user by their username and now has an inactive log out button

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,32 @@
 import React from "react";
+
 import "./style.css";
 import img from "./birb.png";
 
+
+let greeting = window.location.pathname === "/" ? "" : "Hello,  " + sessionStorage.getItem("username");
+
+
+
 function Nav() {
   return (
+
+    
     <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-success">
+      <img src = {img} alt="budgie" height="42" width="42"></img>
       <a className="navbar-brand" href="/">
         Budgie
       </a>
 
-      <img src = {img} alt="budgie" height="42" width="42"></img>
+      
+
+      
+      <div id = "greeting"> {greeting} </div>
+
+      <button
+                                        id="btn-logout"
+                                        type="button"                                    
+                                        className="btn btn-danger">log out</button>
       
     </nav>
   );
